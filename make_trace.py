ba6bf4a5fa99166f16c17c343831e94f82419b60
Eric Pratts <eppgx@missouri.edu>
2026-04-24 10:01:20 -0500

    Trace maker

---
+++ make_trace.py
@@ -0,0 +1,105 @@
+import argparse
+import configparser
+import numpy as np
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-c', '--config', type=str, required=True, help="DRAM configuration file")
+    parser.add_argument('-n', '--hammer_count', type=int, default=30000, help="Number of times to hammer each victim")
+    parser.add_argument('-t', '--target_count', type=int, default=1, help="Number of rows to target")
+    parser.add_argument('-o', '--output', type=str, default='./trace', help="Output file")
+    
+    args = parser.parse_args()
+
+    config = configparser.ConfigParser()
+    config.read(args.config)
+
+    rows = int(config['dram_structure']['rows'])
+    columns = int(config['dram_structure']['columns'])
+    channels = int(config['system']['channels'])
+    channel_size = int(config['system']['channel_size'])
+    bankgroups = int(config['dram_structure']['bankgroups'])
+    banks_per_group = int(config['dram_structure']['banks_per_group'])
+    tRCD = int(config['timing']['tRCD'])        # minimum timing to open a row and access it
+    tRP = int(config['timing']['tRP'])          # precharge time - time required to close a row
+    hammer_spacing = tRCD + tRP                 # Minimum spacing of hammer strikes. Row must be opened, accessed, and closed
+    tick_time_ns = float(config['timing']['tCK'])
+    BL = int(config['dram_structure']['BL'])
+    bus_width = int(config['system']['bus_width'])
+    device_width = int(config['dram_structure']['device_width'])
+    address_mapping = config['system']['address_mapping']
+
+    banks = bankgroups * banks_per_group
+    devices_per_rank = bus_width / device_width
+    page_size = columns * device_width / 8  # page size in bytes
+    megs_per_bank = page_size * (rows / 1024) / 1024
+    megs_per_rank = megs_per_bank * banks * devices_per_rank
+
+    ranks = channel_size / megs_per_rank
+
+    request_size_bytes = bus_width / 8 * BL
+    shift_bits = np.log2(request_size_bytes)
+    col_low_bits = int(np.log2(BL))
+    actual_col_bits = int(np.log2(columns)) - col_low_bits
+
+    field_widths = {}
+    field_widths["ch"] = int(np.log2(channels))
+    field_widths["ra"] = int(np.log2(ranks))
+    field_widths["bg"] = int(np.log2(bankgroups))
+    field_widths["ba"] = int(np.log2(banks_per_group))
+    field_widths["ro"] = int(np.log2(rows))
+    field_widths["co"] = actual_col_bits
+
+    fields = []
+    field_pos = {}
+
+    for i in range(len(address_mapping.strip())//2):
+        fields.append(address_mapping[2*i:2*i+2])
+    
+    pos = 0
+    while fields != []:
+        token = fields.pop()
+        field_pos[token] = pos
+        pos += field_widths[token]
+    
+    ch_pos = field_pos["ch"]
+    ra_pos = field_pos["ra"]
+    bg_pos = field_pos["bg"]
+    ba_pos = field_pos["ba"]
+    ro_pos = field_pos["ro"]
+    co_pos = field_pos["co"]
+
+    ch_mask = (1 << field_widths["ch"]) - 1
+    ra_mask = (1 << field_widths["ra"]) - 1
+    bg_mask = (1 << field_widths["bg"]) - 1
+    ba_mask = (1 << field_widths["ba"]) - 1
+    ro_mask = (1 << field_widths["ro"]) - 1
+    co_mask = (1 << field_widths["co"]) - 1
+
+
+    rng = np.random.default_rng()
+
+    victim_rows = rng.integers(low=1, high=rows - 1, size=args.target_count)
+    victim_columns = rng.integers(low=0, high=columns >> col_low_bits, size=args.hammer_count)
+    clock = 0
+
+    outfile = open(args.output, 'w')
+
+    # For simplicity, the attack will target channel, rank, bg, and bank 0, only the row
+    # and column will change 
+    for r in victim_rows:
+        for c in victim_columns:
+            address = (r - 1 << ro_pos) | (c << co_pos)
+            command = f'0x{address:08x} READ {clock}\n'
+            outfile.write(command)
+
+            address = (r + 1 << ro_pos) | (c << co_pos)
+            command = f'0x{address:08x} READ {clock + 1}\n'
+            outfile.write(command)
+            clock += hammer_spacing
+
+
+
+if __name__ == "__main__":
+    main()
